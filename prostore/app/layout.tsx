ef553e1a5d438310c848bc5d8cf6a96b3b4a9e1b
Jaspher Cabilin Bagor <141024571+jaspherbagor@users.noreply.github.com>
2025-07-25 18:04:21 +0800

    modified the layout

--- prostore/app/layout.tsx
+++ prostore/app/layout.tsx
@@ -1,12 +1,12 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 import '@/assets/styles/globals.css';
 
-const inter = Inter({subsets:['latin']});
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Prostore',
-  description: 'A modern e-commerce platform built with Next.js',
+  description: 'A modern store built with Next.js',
 };
 
 export default function RootLayout({
@@ -15,12 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.className} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang='en' suppressHydrationWarning>
+      <body className={`${inter.className}`}>{children}</body>
     </html>
   );
 }
