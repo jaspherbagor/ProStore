c3302b6043218275bd0a546e201b1bb798b92fc8
Jaspher Bagor <jaspherbagor0357@gmail.com>
2025-07-30 12:02:32 +0800

    updated the imports in the modetoggle

--- prostore/components/shared/header/mode-toggle.tsx
+++ prostore/components/shared/header/mode-toggle.tsx
@@ -1,16 +1,16 @@
 "use client";
-import {useState, useEffect} from "react";
-import { 
-    DropdownMenu, 
-    DropdownMenuTrigger, 
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuContent,
-    DropdownMenuCheckboxItem
-} from "@radix-ui/react-dropdown-menu";
-import { useTheme } from "next-themes";
-import {SunIcon, MoonIcon, SunMoon} from 'lucide-react';
-import { Button } from "@/components/ui/button";
+import { useEffect, useState } from 'react';
+import { useTheme } from 'next-themes';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { MoonIcon, SunIcon, SunMoon } from 'lucide-react';
 const ModeToggle = () => {
     const [mounted, setMounted] = useState(false);
     const {theme, setTheme} = useTheme();
