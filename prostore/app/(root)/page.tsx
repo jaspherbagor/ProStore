1d9b8d84abc4a7f289afd7dade0f0d18cccef98a
Jaspher Bagor <jaspherbagor0357@gmail.com>
2025-08-02 15:12:26 +0800

    added a loader delay functionality

--- prostore/app/(root)/page.tsx
+++ prostore/app/(root)/page.tsx
@@ -1,7 +1,11 @@
 export const metadata = {
   title: 'Home'
 };
-const Homepage = () => {
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const Homepage = async () => {
+  await delay(2000);
   return <>Prostore</> ;
 };
 
