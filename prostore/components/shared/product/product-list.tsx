c6a27e37727359969a40cd8f22ddc8cfffd48522
Jaspher Bagor <jaspherbagor0357@gmail.com>
2025-08-13 21:28:10 +0800

    Added a product card component

--- prostore/components/shared/product/product-list.tsx
+++ prostore/components/shared/product/product-list.tsx
@@ -1,4 +1,13 @@
-const ProductList = ({data, title, limit}: {data:any; title?:string; limit?:number }) => {
+import ProductCard from "./product-card";
+const ProductList = ({
+    data, 
+    title, 
+    limit
+}: {
+    data:any; 
+    title?:string; 
+    limit?:number 
+}) => {
     const limitedData = limit ? data.slice(0, limit) : data;
 
     return ( <div className="my-10">
@@ -6,7 +15,7 @@ const ProductList = ({data, title, limit}: {data:any; title?:string; limit?:numb
         {data.length > 0 ? (
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 {limitedData.map((product:any) => (
-                    <div>{product.name}</div>
+                    <ProductCard key={product.slug} product={product} />
                 ))}
             </div>
         ) : (
