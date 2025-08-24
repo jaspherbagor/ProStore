e1aa44db8f55b5a4eed7fd707d49f027b4d336c9
Jaspher Bagor <jaspherbagor0357@gmail.com>
2025-08-24 14:33:16 +0800

    added the logic of the product actions

---
+++ prostore/lib/actions/product.actions.ts
@@ -0,0 +1,15 @@
+'use server';
+import { PrismaClient } from "../generated/prisma";
+
+
+// get latest products
+export async function getLatestProducts() {
+    const prisma = new PrismaClient();
+
+    const data = await prisma.product.findMany({
+        take: 4,
+        orderBy: {createdAt: 'desc'},
+    });
+
+    return data;
+}
