422f77b9260edb699c42817b1cefea2b515a3aaa
Jaspher Bagor <jaspherbagor0357@gmail.com>
2025-08-24 14:55:46 +0800

    added a dynamic functionality for the number of latest products to be displayed in the home page

--- prostore/lib/actions/product.actions.ts
+++ prostore/lib/actions/product.actions.ts
@@ -1,13 +1,14 @@
 'use server';
 import { PrismaClient } from "../generated/prisma";
 import { convertToPlainObject } from "../utils";
+import { LATEST_PRODUCTS_LIMIT } from "../constants";
 
 // get latest products
 export async function getLatestProducts() {
     const prisma = new PrismaClient();
 
     const data = await prisma.product.findMany({
-        take: 4,
+        take: LATEST_PRODUCTS_LIMIT,
         orderBy: {createdAt: 'desc'},
     });
 
